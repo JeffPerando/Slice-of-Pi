903bf156e58d9fc5675e49794bbe3e65171796bf
iluadiaz20 <iluadiaz20@mail.wou.edu>
2022-02-05 17:56:51 -0800

    Did some minor UI changes

--- Website/Main/Program.cs
+++ Website/Main/Program.cs
@@ -3,6 +3,14 @@ using Microsoft.EntityFrameworkCore;
 using Main.Data;
 using Main.Areas.Identity.Data;
 using MyApplication.Data;
+using System.Data.SqlClient;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,6 +24,7 @@ builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<CrimeDbContext>();
 builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
 
 var app = builder.Build();
 
