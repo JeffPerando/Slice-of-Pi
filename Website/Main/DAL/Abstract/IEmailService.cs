c60987bff49e90e20f96678942b150c6a6dbb266
Jeff Perando <realmsofcaelum@gmail.com>
2022-02-22 20:43:07 -0800

    Implemented tooling for sending plaintext emails

---
+++ Website/Main/DAL/Abstract/IEmailService.cs
@@ -0,0 +1,14 @@
+﻿
+namespace Main.DAL.Abstract
+{
+    public interface IEmailService
+    {
+        void LogIn();
+        
+        void SendTextEmail(string receiver, string receiverName, string subject, string content);
+
+        void LogOut();
+
+    }
+
+}
---
+++ Website/Test/EmailTests.cs
@@ -0,0 +1,69 @@
+﻿
+using NUnit.Framework;
+using Main.DAL.Abstract;
+using Main.DAL.Concrete;
+using Microsoft.Extensions.Configuration;
+using NuGet.Configuration;
+using System;
+
+namespace Test
+{
+    public class EmailTests
+    {
+        private string defaultReceiver = "jbrusseperando18@mail.wou.edu";
+        private IEmailService? email;
+
+        [SetUp]
+        public void Setup()
+        {
+            var configuration = new ConfigurationBuilder()
+            .AddUserSecrets<EmailTests>()
+            .Build();
+
+            System.Diagnostics.Debug.WriteLine(configuration["EmailPW"]);
+            
+            email = new EmailService("Slice of Pi Unit Tests", "sliceofpi.cs46x", configuration["EmailPW"]);
+
+            email.LogIn();
+
+        }
+
+        [TearDown]
+        public void Clean()
+        {
+            if (email != null)
+            {
+                email.LogOut();
+            }
+        }
+
+        [Test]
+        public void Test_SendsEmail()
+        {
+            Assert.DoesNotThrow(() => {
+                email.SendTextEmail(defaultReceiver, "", "Unit testing", "Hi! If you're receiving this, then the unit test passed!");
+            });
+
+        }
+
+        [Test]
+        public void Test_NoEmptyReceiver()
+        {
+            Assert.Throws<ArgumentException>(() => {
+                email.SendTextEmail("", "Mr. Pilkington", "Test_NoEmptyReceiver", "This shouldn't pass...");
+            });
+
+        }
+
+        [Test]
+        public void Test_NoEmptyContent()
+        {
+            Assert.Throws<ArgumentException>(() => {
+                email.SendTextEmail(defaultReceiver, "John Doe", "Test_NoEmptyContent email", "");
+            });
+
+        }
+
+    }
+
+}
