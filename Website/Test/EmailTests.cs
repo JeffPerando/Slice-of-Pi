9209fd0043cc08ee5f2dc4f5d20df53ed7a4864d
Jeff Perando <realmsofcaelum@gmail.com>
2022-02-26 19:25:37 -0800

    Improved email unit testing

--- Website/Test/EmailTests.cs
+++ Website/Test/EmailTests.cs
@@ -8,40 +8,52 @@ using System;
 
 namespace Test
 {
+#pragma warning disable CS8602 // Dereference of a possibly null reference.
     public class EmailTests
     {
         private string defaultReceiver = "jbrusseperando18@mail.wou.edu";
         private IEmailService? email;
+        private string username = "sliceofpi.cs46x";
+        private string password = "";
 
         [SetUp]
         public void Setup()
         {
-            var configuration = new ConfigurationBuilder()
-            .AddUserSecrets<EmailTests>()
-            .Build();
+            if (password.Length == 0)
+            {
+                var configuration = new ConfigurationBuilder()
+                .AddUserSecrets<EmailTests>()
+                .Build();
 
-            System.Diagnostics.Debug.WriteLine(configuration["EmailPW"]);
-            
-            email = new EmailService("Slice of Pi Unit Tests", "sliceofpi.cs46x", configuration["EmailPW"]);
+                password = configuration["EmailPW"];
 
-            email.LogIn();
+            }
+
+            email = new EmailService("Slice of Pi Unit Tests", username, password);
 
         }
 
         [TearDown]
         public void Clean()
         {
-            if (email != null)
+            if (email != null && email.IsLoggedIn())
             {
                 email.LogOut();
+
             }
+
         }
 
         [Test]
-        public void Test_SendsEmail()
+        public void Test_SendsTextEmail()
         {
+            var date = DateTime.Now;
+
+            email.LogIn();
+            
+            Assert.That(email.IsLoggedIn(), Is.True);
             Assert.DoesNotThrow(() => {
-                email.SendTextEmail(defaultReceiver, "", "Unit testing", "Hi! If you're receiving this, then the unit test passed!");
+                email.SendTextEmail(defaultReceiver, "Tester", $"Unit test from {date.ToShortTimeString()}", "Hi! If you're receiving this, then the unit test passed!");
             });
 
         }
@@ -49,6 +61,9 @@ namespace Test
         [Test]
         public void Test_NoEmptyReceiver()
         {
+            email.LogIn();
+
+            Assert.That(email.IsLoggedIn(), Is.True);
             Assert.Throws<ArgumentException>(() => {
                 email.SendTextEmail("", "Mr. Pilkington", "Test_NoEmptyReceiver", "This shouldn't pass...");
             });
@@ -58,12 +73,28 @@ namespace Test
         [Test]
         public void Test_NoEmptyContent()
         {
+            email.LogIn();
+
+            Assert.That(email.IsLoggedIn(), Is.True);
             Assert.Throws<ArgumentException>(() => {
                 email.SendTextEmail(defaultReceiver, "John Doe", "Test_NoEmptyContent email", "");
             });
 
         }
 
+        [Test]
+        public void Test_DoubleLogInFails()
+        {
+            email.LogIn();
+            Assert.That(email.IsLoggedIn(), Is.True);
+            Assert.That(() =>
+            {
+                email.LogIn();
+            }, Throws.Exception);
+
+        }
+
     }
 
 }
+#pragma warning restore CS8602 // Dereference of a possibly null reference.
