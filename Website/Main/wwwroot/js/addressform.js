6c8aba768239ea92143ada9699d01a5f9602a274
Jeff P <realmsofcaelum@gmail.com>
2022-02-28 08:28:09 -0800

    Added user information editing feature

---
+++ Website/Main/wwwroot/js/addressform.js
@@ -0,0 +1,21 @@
+﻿
+$(function () {
+    $.ajax({
+        type: "GET",
+        dataType: "text",
+        url: "/apiv3/forms/address",
+        success: addAddressForm,
+        error: ajaxErr
+
+    });
+
+})
+
+function ajaxErr(xhr, options, err) {
+    console.log("AJAX is in his tent, complaining. Or maybe it's Achilles. Anyway there's an error");
+    console.log(err);
+}
+
+function addAddressForm(data) {
+    $("#addrForm").html(data);
+}
