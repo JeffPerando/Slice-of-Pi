010878a821dc0e33c9a519e8f0f528b6088e8875
Jeff Perando <realmsofcaelum@gmail.com>
2022-03-07 23:23:01 -0800

    Got JS working for popovers

--- Website/Main/wwwroot/js/site.js
+++ Website/Main/wwwroot/js/site.js
@@ -1,7 +1,19 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
-$(function() {
+//please don't touch this import, ty
+import '../lib/bootstrap/dist/js/bootstrap.bundle.js';
+
+$(function () {
+
+    var popovers = [];
+    //don't touch this either
+    $(".popover").each(function (i) {
+        popovers.push(new bootstrap.Popover(this, {
+            trigger: 'focus'
+        }));
+    });
+    
     $.ajax({
         type: "GET",
         dataType: "json",
@@ -35,7 +47,6 @@ function errorOnAjax()
 {
     console.log("ERROR in ajax request");
 }
-
 
 function displayStateInformation(data)
 {
