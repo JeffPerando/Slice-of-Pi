ae1171ca8e8cd8d16c57906873394a6cd7e9becd
Jeff Perando <realmsofcaelum@gmail.com>
2022-03-11 01:49:58 -0800

    Refined helper icon, HTML helper

--- Website/Main/Extensions/TooltipHelper.cs
+++ Website/Main/Extensions/TooltipHelper.cs
@@ -9,7 +9,7 @@ namespace Main.Extensions
         public static IHtmlContent Tooltip(this IHtmlHelper helper, string tooltip, string direction = "top")
         {
             //<a class="sp-popover" data-bs-trigger="hover focus" data-bs-content="Popup with option trigger" data-bs-placement="bottom">Popup with option trigger</a>
-            return new HtmlString(String.Format("<a class='sp-popover' data-bs-trigger='hover focus' data-bs-placement='{1}' data-bs-content='{0}'><img src='que.png' style='width: 16px; height: 16px;'></a>", tooltip, direction));
+            return new HtmlString(String.Format("<span class='sp-popover' data-bs-trigger='hover focus' data-bs-placement='{1}' data-bs-content='{0}'><img src='que.png' style='width: 16px; height: 16px;'></span>", tooltip, direction));
         }
     }
 }
