2c60bf08713d8991031cf9e6b795d1b23f2f437f
Jeff Perando <realmsofcaelum@gmail.com>
2022-03-11 21:28:41 -0800

    Refined JS, added more tooltips

--- Website/Main/wwwroot/js/index.js
+++ Website/Main/wwwroot/js/index.js
@@ -1,6 +1,60 @@
 ﻿//according to loftblog tut
 $('.nav li:first').addClass('active');
 
+$(function () {
+
+    $.ajax({
+        type: "GET",
+        dataType: "json",
+        url: "apiv3/FBI/StateStats",
+        success: displayStateInformation,
+        error: errorOnAjax
+
+    });
+
+    $.ajax({
+        type: "GET",
+        dataType: "json",
+        url: "/apiv3/FBI/StateList",
+        success: populateDropDown,
+        error: errorOnAjax
+
+    });
+
+})
+
+function errorOnAjax() {
+    console.log("ERROR in ajax request");
+}
+
+function displayStateInformation(data) {
+    console.log(data);
+
+    $("#safestStatesTable>tbody").empty();
+    for (let i = 0; i < data.length; ++i) {
+        let repoTR = $(
+            `<tr>
+                <td style="color:white; font-weight:bold;">${data[i]["state"]}</td>
+                <td style="color:white; font-weight:bold;">${data[i]["crime_Per_Capita"]}</td>
+            </tr>`
+        )
+        $("#safestStatesTable>tbody").append(repoTR);
+        $("#safestStatesTable").show();
+    }
+}
+
+function populateDropDown(data) {
+    var select = document.getElementById("stateAbbrev");
+    for (var i = 0; i < data.length; i++) {
+        var option = data[i];
+        var element = document.createElement("option");
+        element.textContent = option;
+        element.value = option;
+        select.appendChild(element);
+    }
+}
+
+
 var showSection = function showSection(section, isAnimate) {
     var
         direction = section.replace(/#/, ''),
