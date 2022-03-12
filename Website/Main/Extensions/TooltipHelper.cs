a4cb54fc7dc61f920fdbe657ec3288703b8dd760
Jeff Perando <realmsofcaelum@gmail.com>
2022-03-11 23:21:31 -0800

    Fixed icon on non-index pages

--- Website/Main/Extensions/TooltipHelper.cs
+++ Website/Main/Extensions/TooltipHelper.cs
@@ -8,7 +8,7 @@ namespace Main.Extensions
     {
         public static IHtmlContent Tooltip(this IHtmlHelper helper, string tooltip, string direction = "top")
         {
-            return new HtmlString(String.Format("<span class='sp-popover' data-bs-trigger='hover focus' data-bs-placement='{1}' data-bs-content='{0}'><img src='que.png' style='width: 16px; height: 16px;'></span>", tooltip, direction));
+            return new HtmlString(String.Format("<span class='sp-popover' data-bs-trigger='hover focus' data-bs-placement='{1}' data-bs-content='{0}'><img src='/que.png' style='width: 16px; height: 16px;'></span>", tooltip, direction));
         }
     }
 }
