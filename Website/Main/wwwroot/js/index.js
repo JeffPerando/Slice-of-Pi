1eed87e311b3ba7d30a003c0329869ee6ed3cdce
Jeff Perando <realmsofcaelum@gmail.com>
2022-03-12 03:24:25 -0800

    Moved styling code out of index.js

--- Website/Main/wwwroot/js/index.js
+++ Website/Main/wwwroot/js/index.js
@@ -1,6 +1,4 @@
-﻿//according to loftblog tut
-$('.nav li:first').addClass('active');
-
+﻿
 $(function () {
     console.log("Loading index JS");
 
@@ -36,6 +34,7 @@ function displayStateInformation(data) {
         let repoTR = $(
             `<tr>
                 <td style="color:white; font-weight:bold;">${data[i]["state"]}</td>
+                <td style="color:white; font-weight:bold;">${data[i]["population"]}</td>
                 <td style="color:white; font-weight:bold;">${data[i]["crime_Per_Capita"]}</td>
             </tr>`
         )
@@ -54,46 +53,3 @@ function populateDropDown(data) {
         select.appendChild(element);
     }
 }
-
-var showSection = function showSection(section, isAnimate) {
-    var
-        direction = section.replace(/#/, ''),
-        reqSection = $('.section').filter('[data-section="' + direction + '"]'),
-        reqSectionPos = reqSection.offset().top - 0;
-
-    if (isAnimate) {
-        $('body, html').animate({
-            scrollTop: reqSectionPos
-        },
-            800);
-    } else {
-        $('body, html').scrollTop(reqSectionPos);
-    }
-
-};
-
-var checkSection = function checkSection() {
-    $('.section').each(function () {
-        var
-            $this = $(this),
-            topEdge = $this.offset().top - 80,
-            bottomEdge = topEdge + $this.height(),
-            wScroll = $(window).scrollTop();
-        if (topEdge < wScroll && bottomEdge > wScroll) {
-            var
-                currentId = $this.data('section'),
-                reqLink = $('a').filter('[href*=\\#' + currentId + ']');
-            reqLink.closest('li').addClass('active').
-                siblings().removeClass('active');
-        }
-    });
-};
-
-$('.main-menu, .responsive-menu, .scroll-to-section').on('click', 'a', function (e) {
-    e.preventDefault();
-    showSection($(this).attr('href'), true);
-});
-
-$(window).scroll(function () {
-    checkSection();
-});
