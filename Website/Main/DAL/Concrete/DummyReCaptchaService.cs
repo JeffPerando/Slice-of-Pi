8bac4767a3d3c28d822ece703eebb60a91a4b370
Jeff Perando <realmsofcaelum@gmail.com>
2022-03-12 18:40:12 -0800

    Implemented reCaptcha server-side support

---
+++ Website/Main/DAL/Concrete/DummyReCaptchaService.cs
@@ -0,0 +1,15 @@
+﻿
+using Main.DAL.Abstract;
+
+namespace Main.DAL.Concrete
+{
+    public class DummyReCaptchaService : IReCaptchaService
+    {
+        public Task<bool> Passes(string response)
+        {
+            return Task.FromResult(true);
+        }
+
+    }
+
+}
