ec7992919d93df8aa633031a7c55ee3490b75e7a
Jeff Perando <realmsofcaelum@gmail.com>
2022-03-30 07:37:06 -0700

    Added state crime search results SQL table

--- Website/Main/Scripts/UP.sql
+++ Website/Main/Scripts/UP.sql
@@ -30,7 +30,7 @@ CREATE TABLE [Crime]
 (
     [ID]                INT PRIMARY   KEY IDENTITY(1,1),
     [Year]              INT           NOT NULL,
-    [State]             NVARCHAR(100)   NOT NULL,
+    [State]             NVARCHAR(100) NOT NULL,
     [OffenseType]       NVARCHAR(100) NOT NULL,
     [TotalOffenses]     INT           NOT NULL, 
     [ActualConvictions] INT           NOT NULL,
@@ -40,6 +40,30 @@ CREATE TABLE [Crime]
 
 );
 
+CREATE TABLE [StateCrimeSearchResult]
+(
+    [ID]                INT             PRIMARY KEY IDENTITY(1,1),
+    [UserID]            NVARCHAR(450)   NOT NULL,
+    [DateSearched]      DATETIME        NOT NULL,
+    [State]             NVARCHAR(100)   NOT NULL,
+    [Year]              INT             NOT NULL,
+    --Crime data below this point
+    [Population]        INT             NOT NULL,
+    [ViolentCrimes]     INT             NOT NULL,
+    [Homicide]          INT             NOT NULL,
+    [Homicide]          INT             NOT NULL,
+    [RapeLegacy]        INT             NOT NULL,
+    [RapeRevised]       INT             NOT NULL,
+    [Robbery]           INT             NOT NULL,
+    [Assault]           INT             NOT NULL,
+    [PropertyCrimes]    INT             NOT NULL,
+    [Burglary]          INT             NOT NULL,
+    [Larceny]           INT             NOT NULL,
+    [MotorVehicleTheft] INT             NOT NULL,
+    [Arson]             INT             NOT NULL
+);
+
 ALTER TABLE [Home]  ADD CONSTRAINT [Home_Fk_User]              FOREIGN KEY  ([UserID])   REFERENCES   [User]              ([ID])  ON DELETE NO ACTION ON UPDATE NO ACTION;
 ALTER TABLE [Home]  ADD CONSTRAINT [Home_Fk_AgencyInformation] FOREIGN KEY  ([AgencyID]) REFERENCES   [AgencyInformation] ([ID])  ON DELETE NO ACTION ON UPDATE NO ACTION;
 ALTER TABLE [Crime] ADD CONSTRAINT [Home_Fk_Crime]             FOREIGN KEY  ([AgencyID]) REFERENCES   [AgencyInformation] ([ID])  ON DELETE NO ACTION ON UPDATE NO ACTION;
+ALTER TABLE [StateCrimeSearchResult] ADD CONSTRAINT [SCSR_Fk_User] FOREIGN KEY ([UserID]) REFERENCES  [User]              ([ID])  ON DELETE NO ACTION ON UPDATE NO ACTION;
