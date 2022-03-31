e0fd93243f276d4734b63e13ee1da7eb97d46d0d
Jeff Perando <realmsofcaelum@gmail.com>
2022-03-31 03:59:18 -0700

    Added ICSVBuilder; makes .CSVs from arbitrary data

---
+++ Website/Main/Services/Abstract/ICSVBuilder.cs
@@ -0,0 +1,12 @@
+﻿
+namespace Main.Services.Abstract
+{
+    public interface ICSVBuilder
+    {
+        ICSVBuilder setColumns(string[] columns);
+
+        void addRow(object[] data);
+
+    }
+
+}
