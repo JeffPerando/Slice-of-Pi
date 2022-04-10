5b6d4eb2f4a58c307e639c5a125dddaa16050936
iluadiaz20 <iluadiaz20@mail.wou.edu>
2022-04-10 14:01:31 -0700

    Adding some test files

--- Website/BDD/Features/HighLightedCities.feature
+++ Website/BDD/Features/HighLightedCities.feature
@@ -2,15 +2,69 @@
 
 A short summary of the feature
 
-Scenario Outline: Visitor or user can select a text field from the suggested cities
+Scenario: Clicking the texts sens you to approriate city crime lookup
+    Given I am on the homepage
+    Then I will be able to find recommended states
+    When I click the text 
+    Then I will be redirected to the StateCrime page
+    And I should see crime statistics for the selected state
+
+Scenario Outline: Visitor are rediretced to Amherst, NH
     Given I am on the Home page
-    And I submit the <IvansText>
+    And I click the text <IvansText>
     Then I am redirected to the <CrimePage> Page
-        And <CityName> is equal to <city>
+        And <IvansText> is equal to <input Value>
 
     Examples: 
-    | city | state |
-    | LakeOswego | OR |
-    | Arlington | TX|
-    | New York City | NY | 
-    | Washington | DC |
+    | input Value |
+    | Amherst, NH |
+
+Scenario Outline: Visitor are rediretced to Sedona, Az
+    Given I am on the Home page
+    And I click the text <IvansText>
+    Then I am redirected to the <CrimePage> Page
+        And <IvansText> is equal to <input Value>
+
+    Examples: 
+    | input Value |
+    | Sedona, Az |
+
+Scenario Outline: Visitor are rediretced to Lake Oswego, OR
+    Given I am on the Home page
+    And I click the text <IvansText>
+    Then I am redirected to the <CrimePage> Page
+        And <IvansText> is equal to <input Value>
+
+    Examples: 
+    | input Value |
+    | Lake Oswego, OR |
+
+Scenario Outline: Visitor are rediretced to Greenwood Village, CO
+    Given I am on the Home page
+    And I click the text <IvansText>
+    Then I am redirected to the <CrimePage> Page
+        And <IvansText> is equal to <input Value>
+
+    Examples: 
+    | input Value |
+    | Greenwood Village, CO |
+
+Scenario Outline: Visitor are rediretced to Evanston, IL
+    Given I am on the Home page
+    And I click the text <IvansText>
+    Then I am redirected to the <CrimePage> Page
+        And <IvansText> is equal to <input Value>
+
+    Examples: 
+    | input Value |
+    | Evanston, IL |
+
+Scenario Outline: Visitor are rediretced to Shaker Heights, OH
+    Given I am on the Home page
+    And I click the text <IvansText>
+    Then I am redirected to the <CrimePage> Page
+        And <IvansText> is equal to <input Value>
+
+    Examples: 
+    | input Value |
+    | Shaker Heights, OH |
---
+++ Website/BDD/StepDefinitions/HighLightedCitiesStepDefinitions.cs
@@ -0,0 +1,93 @@
+using System;
+using TechTalk.SpecFlow;
+
+namespace TestBDD.StepDefinitions
+{
+    [Binding]
+    public class HighLightedCitiesStepDefinitions
+    {
+        [Given(@"I am on the homepage")]
+        public void GivenIAmOnTheHomepage()
+        {
+            throw new PendingStepException();
+        }
+
+        [Then(@"I will be able to find recommended states")]
+        public void ThenIWillBeAbleToFindRecommendedStates()
+        {
+            throw new PendingStepException();
+        }
+
+        [When(@"I click the text")]
+        public void WhenIClickTheText()
+        {
+            throw new PendingStepException();
+        }
+
+        [Then(@"I will be redirected to the StateCrime page")]
+        public void ThenIWillBeRedirectedToTheStateCrimePage()
+        {
+            throw new PendingStepException();
+        }
+
+        [Then(@"I should see crime statistics for the selected state")]
+        public void ThenIShouldSeeCrimeStatisticsForTheSelectedState()
+        {
+            throw new PendingStepException();
+        }
+
+        [Given(@"I am on the Home page")]
+        public void GivenIAmOnTheHomePage()
+        {
+            throw new PendingStepException();
+        }
+
+        [Given(@"I click the text <IvansText>")]
+        public void GivenIClickTheTextIvansText()
+        {
+            throw new PendingStepException();
+        }
+
+        [Then(@"I am redirected to the <CrimePage> Page")]
+        public void ThenIAmRedirectedToTheCrimePagePage()
+        {
+            throw new PendingStepException();
+        }
+
+        [Then(@"<IvansText> is equal to Amherst, NH")]
+        public void ThenIvansTextIsEqualToAmherstNH()
+        {
+            throw new PendingStepException();
+        }
+
+        [Then(@"<IvansText> is equal to Sedona, Az")]
+        public void ThenIvansTextIsEqualToSedonaAz()
+        {
+            throw new PendingStepException();
+        }
+
+        [Then(@"<IvansText> is equal to Lake Oswego, OR")]
+        public void ThenIvansTextIsEqualToLakeOswegoOR()
+        {
+            throw new PendingStepException();
+        }
+
+        [Then(@"<IvansText> is equal to Greenwood Village, CO")]
+        public void ThenIvansTextIsEqualToGreenwoodVillageCO()
+        {
+            throw new PendingStepException();
+        }
+
+        [Then(@"<IvansText> is equal to Evanston, IL")]
+        public void ThenIvansTextIsEqualToEvanstonIL()
+        {
+            throw new PendingStepException();
+        }
+
+        [Then(@"<IvansText> is equal to Shaker Heights, OH")]
+        public void ThenIvansTextIsEqualToShakerHeightsOH()
+        {
+            throw new PendingStepException();
+        }
+    }
+}
