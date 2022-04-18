66c6d5946c077864db6f12a12b3137fe091c9e8d
Jeff Perando <realmsofcaelum@gmail.com>
2022-04-17 17:13:37 -0700

    Added DisplayPrice

---
+++ Website/Main/Models/DisplayPrice.cs
@@ -0,0 +1,74 @@
+﻿
+namespace Main.Models
+{
+    public class DisplayPrice
+    {
+        private decimal _price;
+
+        public DisplayPrice(int init)
+        {
+            _price = Convert.ToDecimal(init);
+        }
+
+        public DisplayPrice(float init)
+        {
+            _price = Convert.ToDecimal(Math.Round(init, 2));
+        }
+
+        public DisplayPrice(double init)
+        {
+            _price = Convert.ToDecimal(Math.Round(init, 2));
+        }
+
+        public override string ToString()
+        {
+            return _price.ToString("C");
+        }
+
+        public static bool operator==(DisplayPrice a, DisplayPrice b)
+        {
+            return a._price == b._price;
+        }
+
+        public static bool operator!=(DisplayPrice a, DisplayPrice b)
+        {
+            return a._price != b._price;
+        }
+
+        public static bool operator>(DisplayPrice a, DisplayPrice b)
+        {
+            return a._price > b._price;
+        }
+
+        public static bool operator<(DisplayPrice a, DisplayPrice b)
+        {
+            return a._price < b._price;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (ReferenceEquals(this, obj))
+            {
+                return true;
+            }
+
+            if (ReferenceEquals(obj, null))
+            {
+                return false;
+            }
+
+            if (obj is DisplayPrice)
+            {
+                return _price == ((DisplayPrice)obj)._price;
+            }
+
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return _price.GetHashCode();
+        }
+    }
+
+}
