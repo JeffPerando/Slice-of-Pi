20fcf1f5fdac3abe35d5968870c6d2276273d162
Jeff Perando <realmsofcaelum@gmail.com>
2022-05-20 13:21:38 -0700

    Added repo link + improved formatting

--- Website/Main/Extensions/LinkExtension.cs
+++ Website/Main/Extensions/LinkExtension.cs
@@ -8,7 +8,7 @@ namespace Main.Extensions
     {
         public static IHtmlContent BrandLink(this IHtmlHelper helper, string brand, string color, string url)
         {
-            return new HtmlString($"<div><button type='button' class='btn btn-dark m-1' style='background-color: {color};'><a target='_blank' href='{url}'><img style='vertical-align: middle; display: inline; width: 16px;' src='https://cdn.jsdelivr.net/npm/simple-icons@v6/icons/{brand.ToLower()}.svg' /><span class='text-dark'> {brand}</span></a></button></div>");
+            return new HtmlString($"<span><button type='button' class='btn btn-dark m-1' style='background-color: {color};'><a target='_blank' href='{url}'><img style='vertical-align: middle; display: inline; width: 16px;' src='https://cdn.jsdelivr.net/npm/simple-icons@v6/icons/{brand.ToLower()}.svg' /><span class='text-dark'> {brand}</span></a></button></span>");
         }
 
     }
