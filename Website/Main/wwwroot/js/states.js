d441e661ecfc13583a4ac8ec33d8834b665f200f
Jeff Perando <realmsofcaelum@gmail.com>
2022-05-26 07:48:28 -0700

    FIX: Street View form no longer disables itself

---
+++ Website/Main/wwwroot/js/states.js
@@ -0,0 +1,28 @@
+﻿
+function errorOnAjax() {
+    console.log("ERROR in ajax request");
+}
+
+$(document).ready(function () {
+    $.ajax({
+        type: "GET",
+        dataType: "json",
+        url: "/api/States",
+        success: populateStateDropdown,
+        error: errorOnAjax
+
+    });
+
+});
+
+function populateStateDropdown(data) {
+    let select = $("#states");
+    for (var i = 0; i < data.length; i++) {
+        let state = data[i];
+        let option = document.createElement("option");
+        option.textContent = state.name;
+        option.value = state.abbrev;
+        option.id = state.name;
+        select.append(option);
+    }
+}
